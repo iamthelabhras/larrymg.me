771d045ae98b10284be3641d3051f64cd7cff023
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-20 19:29:28 -0400

    Old larrymg.me skeleton.

---
+++ public/assets/data/this_week.js
@@ -0,0 +1,51 @@
+export let thisWeekLearning = {
+  subjects: [
+    {
+      subjectName: "Vue.js",
+      subjectBullets: [],
+    }, 
+  ],
+};
+
+export let thisWeekReading = {
+  readings: [
+    {
+      readingTitle: "Vue.js In Action",
+      readingAuthor: "Erik Hanchett",
+      readingAuthorLink: "https://twitter.com/erikch"
+    }
+  ],
+};
+
+
+export let thisWeekWatching = {
+    videos: [
+    { 
+      videoTitle: "The Guard",
+      videoTrailer: "https://larrymg.me/video/watching_this_week.mp4",
+      videoYear: "2011",
+      videoDirector: "McDonagh",
+      videoDirectorLink: "https://en.wikipedia.org/wiki/John_Michael_McDonagh",
+    },
+  ],
+};
+
+export let thisWeekHotTrack = 
+  {
+    trackTitle: "The Lobster",
+    trackTitleLink: "https://www.youtube.com/watch?v=_Scs_z-SIfA",
+    trackArtist: "The Gloaming",
+    trackArtistLink: "https://en.wikipedia.org/wiki/The_Gloaming", 
+    trackVideoLink: "https://larrymg.me/video/hot_track.mp4",
+    trackYear: 2019,
+  };
+
+export let thisWeekHotLaugh =
+  {
+    laughTitle: "The Greatest Story Every Told",
+    laughTitleLink: "https://www.youtube.com/watch?v=3ZUinZz_Q2Q",
+    laughArtist: "Peter O'Toole",
+    laughArtistLink: "https://en.wikipedia.org/wiki/Peter_O%27Toole", 
+    laughVideoLink: "https://larrymg.me/video/hot_laugh.mp4",
+    laughYear: "2010?", 
+  };
