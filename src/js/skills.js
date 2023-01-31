d42580fd7c1222277a36210d226a42afbbfd835b
Labhrás Mac Gabhann <coding@larrymg.me>
2023-01-31 12:55:15 -0500

    Moved technical skills data into exported JS module.

---
+++ src/js/skills.js
@@ -0,0 +1,109 @@
+export {iconPath, languages, libraries, fundamentals, tools };
+
+const iconPath = "./assets/images/my_work/resume/"
+
+const languages = [
+  "Languages",
+  {
+    skillName: "JavaScript",
+    skillLevel: "Intermediate",
+    skillIcon: `${iconPath}javascript.svg`,
+    skillIconAlt: "Vanilla JavaScript icon.",
+  },
+  {
+    skillName: "TypeScript",
+    skillLevel: "Beginner",
+    skillIcon: `${iconPath}typescript.png`,
+    skillIconAlt: "TypeScript icon.",
+  },
+];
+
+const libraries = [
+  "JavaScript Libraries & Frameworks",
+  {
+    skillName: "Astro",
+    skillLevel: "Intermediate",
+    skillIcon: `${iconPath}astro.png`,
+    skillIconAlt: "Astro icon.",
+  },
+  {
+    skillName: "React",
+    skillLevel: "Beginner",
+    skillIcon: `${iconPath}react.svg`,
+    skillIconAlt: "React icon.",
+  },
+  {
+    skillName: "Vue",
+    skillLevel: "Beginner",
+    skillIcon: `${iconPath}vue.png`,
+    skillIconAlt: "Vue icon.",
+  },
+  {
+    skillName: "Express.js",
+    skillLevel: "Beginner",
+    skillIcon: `${iconPath}express_js.png`,
+    skillIconAlt: "Express.js icon.",
+  },
+];
+
+const fundamentals = [
+  "Fundamentals",
+  {
+    skillName: "HTML",
+    skillLevel: "Expert",
+    skillIcon: `${iconPath}html.svg`,
+    skillIconAlt: "HTML5 icon.",
+  },
+  {
+    skillName: "CSS",
+    skillLevel: "Expert",
+    skillIcon: `${iconPath}css-3.svg`,
+    skillIconAlt: "CSS3 icon.",
+  },
+  {
+    skillName: "Sass",
+    skillLevel: "Intermediate",
+    skillIcon: `${iconPath}sass.png`,
+    skillIconAlt: "Sass icon.",
+  },
+  {
+    skillName: "JSON",
+    skillLevel: "Proficient",
+    skillIcon: `${iconPath}json.png`,
+    skillIconAlt: "JSON icon.",
+  },
+  {
+    skillName: "REST",
+    skillLevel: "Beginner",
+    skillIcon: `${iconPath}rest_api.png`,
+    skillIconAlt: "REST API icon.",
+  },
+];
+
+const tools = [
+  "Dev Tools",
+  {
+    skillName: "Neovim",
+    skillLevel: "Intermediate",
+    skillIcon: `${iconPath}neovim.png`,
+    skillIconAlt: "Neovim icon.",
+  },
+  {
+    skillName: "VS Code",
+    skillLevel: "Intermediate",
+    skillIcon: `${iconPath}vs_code.png`,
+    skillIconAlt: "VS Code icon.",
+  },
+  {
+    skillName: "Git",
+    skillLevel: "Intermediate",
+    skillIcon: `${iconPath}git.svg`,
+    skillIconAlt: "Git icon.",
+  },
+  {
+    skillName: "GIMP",
+    skillLevel: "Proficient",
+    skillIcon: `${iconPath}gimp.png`,
+    skillIconAlt: "GIMP icon.",
+  },
+];
